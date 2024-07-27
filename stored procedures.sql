6cdb8d88082aceb9fc5954919e20464511927330
lautirots <lautaro.gonzalezruiz@hotmail.com>
2024-07-27 12:18:42 -0300

    Update stored procedures.sql

--- stored procedures.sql
+++ stored procedures.sql
@@ -1,1 +1,15 @@
 
+
+DELIMITER //
+
+CREATE PROCEDURE AgregarCliente(
+    IN nombre VARCHAR(100),
+    IN telefono VARCHAR(20),
+    IN email VARCHAR(100)
+)
+BEGIN
+    INSERT INTO Clientes (nombre, telefono, email)
+    VALUES (nombre, telefono, email);
+END //
+
+DELIMITER ;
