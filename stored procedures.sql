8f0aeb40a4232d43f7042abbfc21643c26292dfc
lautirots <lautaro.gonzalezruiz@hotmail.com>
2024-07-27 12:20:24 -0300

    Update stored procedures.sql

--- stored procedures.sql
+++ stored procedures.sql
@@ -1,4 +1,8 @@
-
+--levantamos un procedimiento,
+--el cual nos permitira agregar de manera mas facil la insercion de nuevos clientes a nuestra tabla,
+--Lo cual nos garantiza darnos la informacion de la cantidad de comensales que podemos recibir en el establecimiento,
+--Con mayor precision, Asi garantizar una correcta organizacion.
+--(Tablas manpiludas: "clientes")
 
 DELIMITER //
 
