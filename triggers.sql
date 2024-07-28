7522bdc8ef7e58d9510698cc7ead068f230b8e90
lautirots <lautaro.gonzalezruiz@hotmail.com>
2024-07-28 11:25:40 -0300

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -1,1 +1,18 @@
+--Este trigger se activará después de que se inserte un nuevo registro en la tabla DetallesPedido
+--y actualizará la tabla Inventarios, asi disminuir la cantidad del plato correspondiente.
+--esto nos permitira llevar un stock minucioso de nuestra carta.
+
+
+DELIMITER //
+
+CREATE TRIGGER ActualizarInventarioDespuesDeInsertar
+AFTER INSERT ON DetallesPedido
+FOR EACH ROW
+BEGIN
+    UPDATE Inventarios
+    SET cantidad = cantidad - NEW.cantidad
+    WHERE idPlato = NEW.idPlato;
+END //
+
+DELIMITER ;
 
