2594af45ecd8570e2e5576b935e8a494c1206a8f
lautirots <lautaro.gonzalezruiz@hotmail.com>
2024-08-12 13:36:51 -0300

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -16,3 +16,23 @@ END //
 
 DELIMITER ;
 
+
+
+--Actualizar la Fecha de Última Visita en la Tabla de Fidelización
+--Este trigger actualiza la fecha de la última visita en la tabla Clientes_Fidelizacion
+--cada vez que se inserta un nuevo pedido del cliente.
+
+DELIMITER $$
+
+CREATE TRIGGER ActualizarFechaUltimaVisita
+AFTER INSERT ON Pedidos
+FOR EACH ROW
+BEGIN
+    -- Actualizar la fecha de la última visita del cliente
+    UPDATE Clientes_Fidelizacion
+    SET fecha_ultima_visita = NEW.fecha
+    WHERE idCliente = NEW.id_cliente;
+END$$
+
+DELIMITER ;
+
